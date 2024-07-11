063b1b83ced78b1770e1375124fddf4912c614d4
Enes07Kalkan <eneskalkansakiz@gmail.com>
2024-07-11 13:40:07 +0200

    input connection with Firebase

--- js/SignUpLoginScript.js
+++ js/SignUpLoginScript.js
@@ -1,13 +1,50 @@
-    let Name = document.getElementById('SignUp-Name').value;
-    let Email = document.getElementById('SignUp-Email').value;
-    let Password = document.getElementById('SignUp-Password').value;
-    let confirmPassword = document.getElementById('SignUp-ConfirmedPassword').value;
+   
+   const Base_URL = "https://join-262-default-rtdb.europe-west1.firebasedatabase.app/";
 
-
-    function signUp(){
-        console.log(Name);
-        console.log(Email);
-        console.log(Password);
-        console.log(confirmPassword);
+   
+   function onSignUpName(Name) {
+        //deletData("/name");
+        postData("User_ID", {"Name": Name});  
     }
-    
+
+    function onSignUpEmail(Email) {
+        //deletData("/name");
+        postData("User_ID", {"Email": Email});  
+    }
+
+    function onSignUpPasswort(Password) {
+        //deletData("/name");
+        postData("User_ID", {"Password": Password});  
+    }
+
+    function SignUpButtonOnClick() {
+        const Name = document.getElementById('SignUp-Name').value;
+        const Email = document.getElementById('SignUp-Email').value;
+        const Password = document.getElementById('SignUp-Password').value;
+        const confirmPassword = document.getElementById('SignUp-ConfirmPassword').value;
+
+        checkPasswort(Password, confirmPassword, Name, Email,);
+    }
+
+
+    function checkPasswort(Password, confirmPassword, Name, Email) {
+        if (Password === confirmPassword) { 
+        onSignUpEmail(Email);
+        onSignUpName(Name);
+        onSignUpPasswort(Password);
+        } else {
+            alert("Password stimmt nicht überein!")
+
+        }
+    }
+
+    async function postData(path="", data={}){ // funktion um daten zu pushen 
+        let response = await fetch(Base_URL + path +".json",{
+            method: "POST", // methode kann geändert werden in "PUT", "DELETE", "PATCH" ,"GET"
+            header: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        });
+        return responsetoJson =  await response.json();
+     }
