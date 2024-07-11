d4642b2dc8e910f0d18c8f7d4236116e1ac5bd30
Enes07Kalkan <eneskalkansakiz@gmail.com>
2024-07-11 12:59:31 +0200

    struktorierung des projektes

---
+++ js/SignUpLoginScript.js
@@ -0,0 +1,13 @@
+    let Name = document.getElementById('SignUp-Name').value;
+    let Email = document.getElementById('SignUp-Email').value;
+    let Password = document.getElementById('SignUp-Password').value;
+    let confirmPassword = document.getElementById('SignUp-ConfirmedPassword').value;
+
+
+    function signUp(){
+        console.log(Name);
+        console.log(Email);
+        console.log(Password);
+        console.log(confirmPassword);
+    }
+    
