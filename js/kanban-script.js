bd8a7cd217207dd61395e82f669f3a86637cd9a7
MrKasimir <143795794+MrKasimir@users.noreply.github.com>
2024-07-21 19:27:34 +0200

    bug fixes on previous version

--- js/kanban-script.js
+++ js/kanban-script.js
@@ -1,3 +1,5 @@
+// kanban-script.js
+
 let tasks = [{
     'id': 0,
     'category': 'todo',
@@ -49,12 +51,19 @@ let tasks = [{
     'subtasks': 2,
 }
 ];
-let count = 0;
+
+let count = tasks.length;
+
 let currentDraggedElement;
-let addedId = -1;
+let currentDraggedCategory;
+let addedId = tasks.length - 1;
 
 function onloadFunction() {
-    tasks = loadBoardFromLocalStorage(); // Läd die Aufgaben aus dem Local Storage
+    saveBoardAsTasksToLocalStorage();
+    getAddedTasksFromLocalStorage();
+    addLocalStorageToBoard();
+    
+    tasks = loadBoardFromLocalStorage();
     renderByCategory();
 }
 
@@ -64,67 +73,123 @@ function saveBoardAsTasksToLocalStorage() {
 
 function loadBoardFromLocalStorage() {
     const boardTasks = localStorage.getItem('board');
-    return boardTasks ? JSON.parse(boardTasks) : [];
+    if (!boardTasks) {
+        return [];
+    }
+    
+    else {
+        return JSON.parse(boardTasks);
+    }
 }
 
 function renderByCategory() {
     deleteKanbanBoard();
-    tasks.forEach((task, i) => {
-        if (task.title) {
-            updateKanbanBoard(i, task.category);
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].title) {
+            if (tasks[i].category == 'todo') {
+                updateKanbanBoard(i, tasks[i].category);
+            }
+            if (tasks[i].category == 'inProgress') {
+                updateKanbanBoard(i, tasks[i].category);
+            }
+            if (tasks[i].category == 'awaitFeedback') {
+                updateKanbanBoard(i, tasks[i].category);
+            }
+            if (tasks[i].category == 'done') {
+                updateKanbanBoard(i, tasks[i].category);
+            }
         }
-    });
+    }
 }
 
 function deleteKanbanBoard() {
-    ['todo', 'inProgress', 'awaitFeedback', 'done'].forEach(category => {
-        document.getElementById(category).innerHTML = '';
-    });
+    document.getElementById('todo').innerHTML = '';
+    document.getElementById('inProgress').innerHTML = '';
+    document.getElementById('awaitFeedback').innerHTML = '';
+    document.getElementById('done').innerHTML = '';
     document.getElementById('inputField').value = '';
     countCategoryInputs();
 }
 
 function updateKanbanBoard(i, category) {
-    document.getElementById(category).innerHTML += `
-        <div class="task-container" draggable="true" ondragstart="startDragging(${tasks[i].id})">
-            <div class="task-titlecategory">${tasks[i].titleCategory}</div>
-            <div class="task-title">${tasks[i].title}</div>
-            <div class="task-description">${tasks[i].description}</div>
-            <div class="task-subtask">${tasks[i].subtasks} / 2 subtasks</div>
-            <div class="task-assignee">${tasks[i].assignedTo}</div>
-        </div>`;
+    document.getElementById(category).innerHTML +=
+        `<div class="task-container" draggable="true" ondragstart="startDragging(${tasks[i].id})">
+    <div class="task-titlecategory">${tasks[i].titleCategory}</div>
+    <div class="task-title">${tasks[i].title}</div>
+    <div class="task-description">${tasks[i].description}</div>
+    <div class="task-subtask">${tasks[i].subtasks} / 2 subtasks</div>
+    <div class="task-assignee">${tasks[i].assignedTo}</div>
+    </div>`;
+}
+
+function moveTo(id) {
+    // Tasks verschieben
 }
 
 function moveTo(event) {
     event.preventDefault();
-    const categoryId = event.target.id;
-    if (categoryId && currentDraggedElement !== undefined) {
-        tasks[currentDraggedElement].category = categoryId;
-        saveBoardAsTasksToLocalStorage(); // Speichere die Änderungen im Local Storage
-        deleteKanbanBoard();
-        renderByCategory();
+    const id = event.target.id;
+    if (id) {
+        tasks[currentDraggedElement].category = id;
+
     }
     event.target.classList.remove('highlight');
+    deleteKanbanBoard();
+    renderByCategory();
+    saveBoardAsTasksToLocalStorage();
 }
 
 function startDragging(id) {
     currentDraggedElement = id;
+    console.log(currentDraggedElement);
 }
 
 function allowDrop(event) {
     event.preventDefault();
+    const id = event.target.id;
+    console.log(`Currently dragging over: ${id}`);
+    if (id) { tasks[currentDraggedElement].category = id; }
     event.target.classList.add('highlight');
 }
 
 function removeHighlight(event) {
+    const id = event.target.id;
+    console.log(`remove highlight: Drag-ID: ${id}`);
+    if (id) { tasks[currentDraggedElement].category = id; }
     event.target.classList.remove('highlight');
 }
 
-function createTask() {
+/* document.addEventListener('DOMContentLoaded', (event) => {
+    function createTask() {
+        let task = document.getElementById('inputField').value;
+        if (task.trim() !== '') {
+            addedId++;
+            tasks.push({
+                'id': addedId,
+                'category': 'todo',
+                'title': task,
+                'titleCategory': 'enter title category',
+                'description': 'enter description',
+                'priority': 'set priority',
+                'assignedTo': 'EF',
+                'subtasks': 0,
+            });
+        }
+        deleteKanbanBoard();
+        renderByCategory();
+    }
+    document.getElementById('inputField').addEventListener('keypress', function (event) {
+        if (event.key === 'Enter') {
+            createTask();
+        }
+    });
+}); */
+
+/* function createTask() {
     let task = document.getElementById('inputField').value;
     if (task.trim() !== '') {
         addedId++;
-        const newTask = {
+        tasks.push({
             'id': addedId,
             'category': 'todo',
             'title': task,
@@ -133,22 +198,11 @@ function createTask() {
             'priority': 'set priority',
             'assignedTo': 'EF',
             'subtasks': 0,
-        };
-        tasks.push(newTask);
-        saveBoardAsTasksToLocalStorage();
-        deleteKanbanBoard();
-        renderByCategory();
+        });
     }
-}
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    onloadFunction();
-    document.getElementById('inputField').addEventListener('keypress', function (event) {
-        if (event.key === 'Enter') {
-            createTask();
-        }
-    });
-});
+    deleteKanbanBoard();
+    renderByCategory();
+} */
 
 let numberTodos = 0;
 let numberInProgress = 0;
@@ -156,23 +210,75 @@ let numberAwaitFeedback = 0;
 let numberDone = 0;
 
 function countCategoryInputs() {
-    ['todo', 'inProgress', 'awaitFeedback', 'done'].forEach(category => {
-        numberTodos += tasks.filter(task => task.category === category).length;
-        console.log(category, numberTodos);
-        numberTodos = 0;
-    });
-    // Render empty categories if needed
-    ['todo', 'inProgress', 'awaitFeedback', 'done'].forEach(category => {
-        if (tasks.filter(task => task.category === category).length === 0) {
-            renderEmptyCategory(category);
+    for (let i = 0; i < tasks.length; i++) {
+        let currentCategory = tasks[i].category;
+        if (currentCategory == 'todo') {
+            numberTodos++;
         }
-    });
+        if (currentCategory == 'inProgress') {
+            numberInProgress++;
+        }
+        if (currentCategory == 'awaitFeedback') {
+            numberAwaitFeedback++;
+        }
+        if (currentCategory == 'done') {
+            numberDone++;
+        }
+    }
+    console.log(numberTodos);
+    console.log(numberInProgress);
+    console.log(numberAwaitFeedback);
+    console.log(numberDone);
+
+    if (numberTodos == 0) {
+        renderEmptyCategoy('todo');
+    }
+    if (numberInProgress == 0) {
+        renderEmptyCategoy('inProgress');
+    }
+    if (numberAwaitFeedback == 0) {
+        renderEmptyCategoy('awaitFeedback');
+    }
+    if (numberDone == 0) {
+        renderEmptyCategoy('done');
+    }
+    numberTodos = 0;
+    numberInProgress = 0;
+    numberAwaitFeedback = 0;
+    numberDone = 0;
 }
 
-function renderEmptyCategory(category) {
-    document.getElementById(category).innerHTML += `
-        <div class="task-container">
-            <div class="task-title">currently empty</div>
-        </div>`;
+function renderEmptyCategoy(category) {
+    document.getElementById(category).innerHTML +=
+        `<div class="task-container">
+    <div class="task-title">currently empty</div>
+    </div>`;
 }
-////////////////////////    Acces Local Storage von Add Task Seite - END ///////////////////////////////////
+
+function getAddedTasksFromLocalStorage() {
+    const storedTasks = localStorage.getItem('tasks');
+    if (!storedTasks) {
+        return [];
+    }
+    else return JSON.parse(storedTasks);
+}
+
+function addLocalStorageToBoard() {
+    const localStorageTasks = getAddedTasksFromLocalStorage();
+    for (let i = 0; i < localStorageTasks.length; i++) {
+        tasks.push(
+            {
+                'id': count,
+                'category': 'todo',
+                'title': localStorageTasks[i]['title'],
+                'titleCategory': 'NEW',
+                'description': 'NEW',
+                'priority': 'NEW',
+                'assignedTo': 'NEW',
+                'subtasks': 99,
+            }
+        );
+        count++;
+    }
+    saveBoardAsTasksToLocalStorage();
+}
