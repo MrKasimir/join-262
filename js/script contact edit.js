14e26b9be49ec22836ea3a79cbb30f5f49f0c6f8
Bitnowa AG <info@bitnowa.ch>
2024-07-23 18:14:18 +0200

    Update script contact edit.js

--- js/script contact edit.js
+++ js/script contact edit.js
@@ -1,3 +1,22 @@
+// Seite wird momentan nicht genutzt aus diesem Grund ist dieser Code erstmal nicht relavant
+
+window.onload = function() {
+    // URL-Parameter extrahieren
+    let params = new URLSearchParams(window.location.search);
+    let name = params.get('name');
+    let email = params.get('email');
+    let number = params.get('number');
+    let image = params.get('image');
+
+    // Füllen Sie die Eingabefelder aus
+    document.getElementById('contactEditInputName').value = decodeURIComponent(name);
+    document.getElementById('contactEditInputEmail').value = decodeURIComponent(email);
+    document.getElementById('contactEditInputNumber').value = decodeURIComponent(number);
+    document.getElementById('profilImg').src = decodeURIComponent(image);
+};
+
+//-------------------- Kein Backend ------------------------//
+
 function previewImage() {
     var file = document.getElementById('imageInput').files[0];
     var reader = new FileReader();
